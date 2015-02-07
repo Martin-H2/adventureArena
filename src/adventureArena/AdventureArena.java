33b3de0ab7e0d7d015fb7317dcd7b77791c2454c
martin <martin.haut@arcor.de>
2015-02-07 21:27:02 +0100

    bla

--- src/adventureArena/AdventureArena.java
+++ src/adventureArena/AdventureArena.java
@@ -4,4 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class AdventureArena extends JavaPlugin {
 
+	@Override
+	public void onEnable() {
+		super.onEnable();
+		getServer().getPluginManager().registerEvents(new AdventureEvents(), this);
+	}
+
 }
---
+++ src/adventureArena/AdventureEvents.java
@@ -0,0 +1,7 @@
+package adventureArena;
+
+import org.bukkit.event.Listener;
+
+public class AdventureEvents implements Listener {
+
+}
